4187bbd67e965598c864bad1490e8dfbe3b99276
Nemanja Andrejevic <nemanja@nmdesign.rs>
2015-10-11 23:32:47 +0200

    Trying to make MySQL load env variables

--- centos6/mysql56/docker-entrypoint.sh
+++ centos6/mysql56/docker-entrypoint.sh
@@ -72,7 +72,7 @@ if [ "$1" = 'mysqld' ]; then
 		fi
 
 		if [ "$MYSQL_SCHEMA" ]; then
-			echo source $MYSQL_SCHEMA
+			"${mysql[@]}" < "$MYSQL_SCHEMA" 
 		fi
 
 		if ! kill -s TERM "$pid" || ! wait "$pid"; then
